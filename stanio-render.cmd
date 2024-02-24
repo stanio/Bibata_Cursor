7536e9220d43569b891711f0a56356d5afd94303
Stanimir Stamenkov <stanio@yahoo.com>
2024-02-24 09:57:00 +0200

    stanio-render: Allow specifying non-default themes
        copy render.json render-custom.json
    
    Edit render-custom.json as necessary, then:
    
        set RENDER_CFG=render-custom.json && stanio-render ^
        	--standard-sizes --windows-cursors
    
    Added render-extra.json to produce ful1e5/Bibata_Extra_Cursor.  Note, one
    may filter/select the rendered themes like:
    
        set RENDER_CFG=render-extra.json && stanio-render ^
        	--standard-sizes --windows-cursors ^
        	-t Bibata-Modern-Turquoise -t Bibata-Original-Pink
    
    Experimental options may be set via RENDER_JOPTS environment variable:
    
        set RENDER_JOPTS=-Dbibata.minAnimSize=48 -Dbibata.maxAnimSize=96
        stanio-render --standard-sizes --windows-cursors

--- stanio-render.cmd
+++ stanio-render.cmd
@@ -1,4 +1,6 @@
 @echo off
 rem # Usual options include: --standard-sizes --windows-cursors
 rem # Use --help for more
-stanio-misc\gradlew -p stanio-misc -q bibata:run --args="render ..\.. %*"
+stanio-misc\gradlew -p stanio-misc -q bibata:run ^
+	"-PbibataJvmArgs=%RENDER_JOPTS%" ^
+	--args="render ..\..\%RENDER_CFG% %*"
