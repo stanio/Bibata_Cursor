1c11d3367f0831c05aa5d329ba98ac95d8afdf26
Stanimir Stamenkov <stanio@yahoo.com>
2024-03-12 10:25:59 +0200

    Check in my release configurations/scripts
      + Update stanio-misc submodule to current
    
            git submodule update --remote

---
+++ stanio-render-linux.cmd
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+set RENDER_ARGS=^
+    -t Bibata-Modern-Classic -t Bibata-Modern-Classic-Thin ^
+    -t Bibata-Modern-Ice -t Bibata-Modern-Ice-Thin ^
+    -t Bibata-Original-Classic -t Bibata-Original-Classic-Thin ^
+    -t Bibata-Original-Ice -t Bibata-Original-Ice-Thin ^
+    --linux-cursors -s R -r 36,48,72,96,120,144
+::  --linux-cursors      -r 24,32,48,64,80,96
+::  --linux-cursors -s R -r 24,30,33,36,42,48,60,72,84,96,120,144
+::  --linux-cursors      -r 16,20,22,24,28,32,40,48,56,64,80,96
+
+:: The default opacity=0.5 appears too dark (less translucent).
+::set SHADOW_ARG=--pointer-shadow=3,21,9
+set SHADOW_ARG=--pointer-shadow=3,21,9,0.25
+
+set RENDER_CONFIG=render-linux.json
+set RENDER_JOPTS=-Dbibata.maxAnimSize=192
+
+set RENDER_SCRIPT="%~dp0stanio-render"
+
+echo === Plain (no Shadow)
+call %RENDER_SCRIPT% %RENDER_ARGS% || exit /b
+
+echo:
+echo === w/ Shadow
+%RENDER_SCRIPT% %RENDER_ARGS% %SHADOW_ARG%
--- stanio-render.cmd
+++ stanio-render.cmd
@@ -1,6 +1,11 @@
 @echo off
-rem # Usual options include: --standard-sizes --windows-cursors
-rem # Use --help for more
-stanio-misc\gradlew -p stanio-misc -q bibata:run ^
-	"-PbibataJvmArgs=%RENDER_JOPTS%" ^
-	--args="render ..\..\%RENDER_CFG% %*"
+:: Usual options include: --standard-sizes --windows-cursors
+:: Use --help for more
+
+setlocal
+set BASEDIR=%~dp0
+set BASEDIR=%BASEDIR:~0,-1%
+
+"%BASEDIR%\stanio-misc\gradlew" -p "%BASEDIR%\stanio-misc" ^
+	-q bibata:run "-PbibataJvmArgs=%RENDER_JOPTS%" ^
+	"--args=render ..\..\%RENDER_CONFIG% %*"
