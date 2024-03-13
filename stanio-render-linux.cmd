4159f223a33621f3e9887924665b99e765a211d6
Stanimir Stamenkov <stanio@yahoo.com>
2024-03-13 12:08:10 +0200

    Use a different output directory layout for Windows
    Have a single Bibata/ top-level directory with all themes/variants as
    children.

--- stanio-render-linux.cmd
+++ stanio-render-linux.cmd
@@ -2,14 +2,19 @@
 setlocal
 
 set RENDER_ARGS=^
-    -t Bibata-Modern-Classic -t Bibata-Modern-Classic-Thin ^
-    -t Bibata-Modern-Ice -t Bibata-Modern-Ice-Thin ^
-    -t Bibata-Original-Classic -t Bibata-Original-Classic-Thin ^
-    -t Bibata-Original-Ice -t Bibata-Original-Ice-Thin ^
-    --linux-cursors -s R -r 36,48,72,96,120,144
-::  --linux-cursors      -r 24,32,48,64,80,96
-::  --linux-cursors -s R -r 24,30,33,36,42,48,60,72,84,96,120,144
-::  --linux-cursors      -r 16,20,22,24,28,32,40,48,56,64,80,96
+    -t Bibata-Modern-Classic ^
+    -t Bibata-Modern-Classic-Thin ^
+    -t Bibata-Modern-Ice ^
+    -t Bibata-Modern-Ice-Thin ^
+    -t Bibata-Original-Classic ^
+    -t Bibata-Original-Classic-Thin ^
+    -t Bibata-Original-Ice ^
+    -t Bibata-Original-Ice-Thin ^
+    --linux-cursors ^
+    -s R -r 36,48,72,96,120,144
+::       -r 24,32,48,64,80,96
+::  -s R -r 24,30,33,36,42,48,60,72,84,96,120,144
+::       -r 16,20,22,24,28,32,40,48,56,64,80,96
 
 :: The default opacity=0.5 appears too dark (less translucent).
 ::set SHADOW_ARG=--pointer-shadow=3,21,9
--- stanio-render-windows.cmd
+++ stanio-render-windows.cmd
@@ -2,34 +2,35 @@
 setlocal
 
 set RENDER_ARGS=^
-    -t Bibata-Modern-Classic -t Bibata-Modern-Classic-Thin ^
-    -t Bibata-Modern-Ice -t Bibata-Modern-Ice-Thin ^
-    -t Bibata-Original-Classic -t Bibata-Original-Classic-Thin ^
-    -t Bibata-Original-Ice -t Bibata-Original-Ice-Thin ^
-    --windows-cursors -r 32,48,64,96,128
+    -t Bibata-Modern-Classic ^
+    -t Bibata-Modern-Classic-Thin ^
+    -t Bibata-Modern-Ice ^
+    -t Bibata-Modern-Ice-Thin ^
+    -t Bibata-Original-Classic ^
+    -t Bibata-Original-Classic-Thin ^
+    -t Bibata-Original-Ice ^
+    -t Bibata-Original-Ice-Thin ^
+    --windows-cursors ^
+    -r 32,48,64,96,128
 
 :: The default opacity=0.5 appears too dark (less translucent).
 ::set SHADOW_ARG=--pointer-shadow=3,21,9
 set SHADOW_ARG=--pointer-shadow=3,21,9,0.25
 
 set RENDER_CONFIG=render-windows.json
+set RENDER_JOPTS=-Dbibata.maxAnimSize=96
 
 set RENDER_SCRIPT="%~dp0stanio-render"
 
-echo === Regular
-set RENDER_JOPTS=-Dbibata.maxAnimSize=96
-call %RENDER_SCRIPT% %RENDER_ARGS% -s R || exit /b
+echo === Regular and Large
+call %RENDER_SCRIPT% %RENDER_ARGS% -s R,L || exit /b
 
 echo:
-echo === Regular with Shadow
-call %RENDER_SCRIPT% %RENDER_ARGS% -s R %SHADOW_ARG% || exit /b
+echo === Regular and Large with Shadow
+call %RENDER_SCRIPT% %RENDER_ARGS% -s R,L %SHADOW_ARG% || exit /b
 
 echo:
-echo === Large and Extra-Large
+echo === Extra-Large
 set RENDER_JOPTS=-Dbibata.maxAnimSize=64
-call %RENDER_SCRIPT% %RENDER_ARGS% -s L,XL || exit /b
-
-echo:
-echo === Large with Shadow
+%RENDER_SCRIPT% %RENDER_ARGS% -s XL
 :: No Extra-Large with Shadow
-%RENDER_SCRIPT% %RENDER_ARGS% -s L %SHADOW_ARG%
